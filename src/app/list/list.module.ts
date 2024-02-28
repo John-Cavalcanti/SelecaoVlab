84c491441d01e87d2d35f5bdfedbac98d4356763
João Victor Cavalcanti <jvictorao123@gmail.com>
2024-02-27 22:17:50 -0300

    criação de nova página

--- src/app/list/list.module.ts
+++ src/app/list/list.module.ts
@@ -4,13 +4,15 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { GamesListingComponent } from './games-listing/games-listing.component';
 import { GameCardComponent } from './game-card/game-card.component';
 import { HttpClientModule } from '@angular/common/http';
+import { GamePageComponent } from './game-page/game-page.component';
 
 
 @NgModule({
   declarations: [
     HomepageComponent,
     GamesListingComponent,
-    GameCardComponent
+    GameCardComponent,
+    GamePageComponent
   ],
   imports: [
     CommonModule,
